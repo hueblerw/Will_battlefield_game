315b2989600f900115d3d7fac1470f996640828c
William Huebler <hueblerw@gmail.com>
2016-09-17 09:56:30 -0500

    Create leader class

---
+++ javascript/classes/leader.js
@@ -0,0 +1,6 @@
+var Unit = function(name, charisma, militarySkill, experience) {
+  	this.name = name;
+  	this.charisma = charisma;
+  	this.militarySkill = militarySkill;
+  	this.experience = experience;
+}
